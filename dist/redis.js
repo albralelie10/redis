0643247c22d855ec2ec7eb4c05ef92c137d95781
polo <polo@contoso.com>
2023-09-14 02:15:23 -0600

    ddf

--- dist/redis.js
+++ dist/redis.js
@@ -4,9 +4,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const ioredis_1 = __importDefault(require("ioredis"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config(); // Carga las variables de entorno desde el archivo .env
 const redisClient = new ioredis_1.default({
-    host: process.env.PORT || 'localhost',
-    port: 6379,
+    host: process.env.REDIS_HOST || 'localhost',
+    port: parseInt(process.env.PORT) || 6379,
     // Otras opciones de configuración de conexión si es necesario
 });
 exports.default = redisClient;
