7ec56309f1053d09d424aa6cc9506f09488d5e68
polo <polo@contoso.com>
2023-09-14 02:14:57 -0600

    redis ifx

--- src/redis.ts
+++ src/redis.ts
@@ -1,9 +1,12 @@
 import Redis from 'ioredis';
+import dotenv from 'dotenv';
+
+dotenv.config(); // Carga las variables de entorno desde el archivo .env
 
 const redisClient = new Redis({
-    host: process.env.PORT||'localhost',
-    port: 6379,
-    // Otras opciones de configuración de conexión si es necesario
-  });
-  
-  export default redisClient;
+  host: process.env.REDIS_HOST || 'localhost',
+  port: parseInt(process.env.PORT) || 6379,
+  // Otras opciones de configuración de conexión si es necesario
+});
+
+export default redisClient;
